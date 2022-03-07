f0620245d978864ad89890d9e5a2153b180e671c
kmitiko <kmitiko1@gmail.com>
2022-03-07 17:13:32 -0300

    mudanca de conexao

--- conexao.php
+++ conexao.php
@@ -1,13 +1,26 @@
 <?php
 
 
-try {
-    $pdo = new PDO("mysql:dbname=loja;host=localhost","root", "");
+define("__HOST__", "localhost");
+define("__USER__", "root");
+define("__PASS__", "");
+define("__DATABASE__", "loja");
 
-} catch (PDOException $th) {
-    echo "Erro com o banco de dados: " . $th;
+function getConnection()
+{
+    try {
+        $key = 'strval';
+        $con = new PDO("mysql:host={$key(__HOST__)}; dbname={$key(__DATABASE__)}", __USER__, __PASS__)
+            or die("Error when trying to connect to the database");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $con;
+    } catch (PDOException $error) { # caso não consiga conectar irá entrar no  bloco do catch
+        echo "Error when connect to database. Erro: {$error->getMessage()}";
+   
+exit;
+    }
 }
-catch (Exception $th) {
-    echo "Erro com o banco de dados: " . $th;
-}
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+getConnection();
+
